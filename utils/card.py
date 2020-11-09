823d0702be0df911a361a50c13d463a0144fbccc
unknown <gemisguillaume@gmail.com>
2020-11-09 13:48:39 +0100

    First version

--- utils/card.py
+++ utils/card.py
@@ -1,1 +1,44 @@
 
+class Symbol:
+    
+    def __init__(self):
+        
+        self.color = ["black", "red"]
+        self.icon = ["♥","♦","♣","♠"]
+        
+class Card(Symbol):
+    
+    def __init__(self):
+        
+        col = Symbol()
+        self.colors = col.color
+        self.icons = col.icon
+        self.value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        
+        
+    def fill_deck(self):
+        
+        dic = {}
+        h = 0
+        for i in range(len(self.value)):
+            for j in self.icons: 
+                
+                if j == "♥" or j == "♦":
+                    sym = self.colors[1]
+                else:
+                    sym = self.colors[0]
+                
+                dic[h] = {"value" : self.value[i], "icon" : j, "color" : sym}
+                h += 1
+                
+        return dic
+    
+    def shuffle(self):
+        
+        deck = fill_deck()
+        
+    
+    
+
+
+
--- utils/player.py
+++ utils/player.py
@@ -1,1 +1,76 @@
+from card import Card
+import random
+
+
+class Player:
+    
+    def __init__(self):
+        
+        obj = Card()
+        self.cards = obj.fill_deck()
+        self.turn_count = 0
+        self.history = {}
+        self.number_of_cards = len(self.history)
+
+        
+    def play(self):
+        
+        ran = random.randint(0, len(self.cards))
+        self.history = {self.cards[ran]}
+        print("{} {} played: {} {}".format(player_name, self.turn_count, dic[ran]["value"], dic[ran]["icon"]))
+        #self.cards.pop(self.cards[ran])
+        
+        return self.cards[ran]
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+obj = Card()
+print(obj.icons)
+
+
+
+
+value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+color = ["black", "red"]
+icon = ["♥","♦","♣","♠"]
+
+dic = {}
+h = 0
+for i in range(len(value)):
+    for j in icon: 
+        
+        if j == "♥" or j == "♦":
+            sym = color[1]
+        else:
+            sym = color[0]
+        
+        dic[h] = {"value" : value[i], "icon" : j, "color" : sym}
+        h += 1
+print(dic[2]["value"])
+
+print(len(dic))
+ran = random.randint(0, len(dic))
+print(ran)
+import random
+
+random.shuffle(dic)
+print(dic)
+
 
